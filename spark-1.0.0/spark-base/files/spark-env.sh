90aeabded3a23b0d4fef49ad7818e8c6ca3103e5
LuqmanSahaf <lgsahaf@gmail.com>
2014-10-11 15:41:58 +0500

    debug

--- spark-1.0.0/spark-base/files/spark-env.sh
+++ spark-1.0.0/spark-base/files/spark-env.sh
@@ -2,9 +2,9 @@
 export SCALA_HOME=/opt/scala-2.10.3
 export SPARK_HOME=__SPARK_HOME__
 export SPARK_WORKER_CORES=1
-export SPARK_MEM=250m
-export SPARK_WORKER_MEMORY=800m
-export SPARK_MASTER_MEM=800m
+export SPARK_MEM=500m
+export SPARK_WORKER_MEMORY=750m
+export SPARK_MASTER_MEM=500m
 export SPARK_MASTER_IP=__MASTER__
 export HADOOP_HOME="/etc/hadoop"
 export MASTER="spark://__MASTER__:7077"
