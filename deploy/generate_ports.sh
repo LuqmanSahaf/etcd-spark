89bf27c0bfa8c6d6edae20e5bc34b7af9e094576
LuqmanSahaf <LuqmanSahaf@github.com>
2014-10-23 10:35:06 +0000

    fixed generate_ports.sh

--- deploy/generate_ports.sh
+++ deploy/generate_ports.sh
@@ -46,7 +46,7 @@ cat master/config | {
 
         # Put the files in etcd server
         curl -L -XPUT "${default_url}/driver${i}/spark_defaults" --data-urlencode value@driver${i}/spark-defaults.conf
-        curl -L -XPUT "${default_url}/driver${i}/to_publish" -d value=\"${to_publish}\"
+        curl -L -XPUT "${default_url}/driver${i}/to_publish" -d value="$to_publish"
 
         PORT=$(( $PORT +  ($i + 1) * 7 ))
     done
@@ -65,7 +65,7 @@ cat master/config | {
             to_publish="$to_publish ${executor[$k]} ${blockManager[$k]}"
         done
 
-        curl -L -XPUT "$default_url/worker$j/to_publish" -d value=\"${to_publish}\"
+        curl -L -XPUT "$default_url/worker$j/to_publish" -d value="$to_publish"
         curl -L -XPUT "$default_url/worker$j/WORKER_UI" -d value=${workerUI[$j]}
         curl -L -XPUT "$default_url/worker$j/WORKER_PORT" -d value=${worker[$j]}
         curl -L -XPUT "$default_url/worker$j/DATANODE_PORT" -d value=${datanode[$j]}
