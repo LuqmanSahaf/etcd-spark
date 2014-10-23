75ca68ca2e8907b1fe4a85969bfd9b0d336f73bc
LuqmanSahaf <lgsahaf@gmail.com>
2014-10-23 17:06:15 +0500

    fixed worker image

--- spark-1.1.0/spark-worker/files/run_spark_worker.sh
+++ spark-1.1.0/spark-worker/files/run_spark_worker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 . ${SPARK_HOME}/conf/spark-env.sh
-${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER --webui-port $SPARK_WORKER_UI_PORT
+${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_UI_PORT $MASTER
