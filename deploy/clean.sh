35ef5ce01956507a85d03b918914fe7f5696ebfa
LuqmanSahaf <lgsahaf@gmail.com>
2014-10-23 14:44:28 +0500

    fixed clean.sh, generate_ports.sh

--- deploy/clean.sh
+++ deploy/clean.sh
@@ -20,7 +20,6 @@ for (( i=1 ; i<=drivers ; i++ ))
 do
     curl -L -X DELETE $default_url/driver$i/spark_env
     curl -L -X DELETE $default_url/driver$i/to_publish
-    curl -L -X DELETE $default_url/driver$i/log4j
     curl -L -X DELETE $default_url/driver$i/spark_defaults
 done
 
@@ -28,11 +27,14 @@ for (( i=1 ; i<=workers ; i++ ))
 do
     curl -L -X DELETE $default_url/worker$i/spark_env
     curl -L -X DELETE $default_url/worker$i/to_publish
-    curl -L -X DELETE $default_url/worker$i/log4j
     curl -L -X DELETE $default_url/worker$i/DATANODE_PORT
     curl -L -X DELETE $default_url/worker$i/WORKER_UI
     curl -L -X DELETE $default_url/worker$i/WORKER_PORT
 done
 
+curl -L DELETE $default_url/spark_env
+curl -L DELETE $default_url/to_publish
+curl -L DELETE $default_url/name
+curl -L DELETE $default_url/log4j
 curl -L -X DELETE $default_url/workers
 curl -L -X DELETE $default_url/drivers
--- deploy/load_config.sh
+++ deploy/load_config.sh
@@ -6,8 +6,8 @@ drivers=0
 workers=0
 
 # load config file for master
+declare -A master_config
 cat master/config | {
-    declare -A master_config
     while read key value; do
         master_config[$key]=$value
     done
@@ -18,8 +18,8 @@ cat master/config | {
     curl -L $default_url/${master_config["name"]}/name -XPUT -d value=${master_config["name"]}
     curl -L $default_url/${master_config["name"]}/spark_env -XPUT --data-urlencode value@master/spark-env.sh
     curl -L $default_url/${master_config["name"]}/log4j -XPUT --data-urlencode value@log4j.properties
-    curl -L $default_url/${master_config["name"]}/drivers -XPUT -d value=${master_config["drivers"]}
-    curl -L $default_url/${master_config["name"]}/workers -XPUT -d value=${master_config["workers"]}
+    curl -L $default_url/${master_config["name"]}/drivers -XPUT -d value=$drivers
+    curl -L $default_url/${master_config["name"]}/workers -XPUT -d value=$workers
 
     for (( i=1; i<=$drivers ; i++ ))
     do
