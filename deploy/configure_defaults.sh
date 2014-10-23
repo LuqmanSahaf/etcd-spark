7a9519cf6f79708e9e3e1f1cf2de218cd8c8e20a
LuqmanSahaf <lgsahaf@gmail.com>
2014-10-23 13:01:05 +0500

    testing

--- deploy/configure_defaults.sh
+++ deploy/configure_defaults.sh
@@ -10,9 +10,11 @@ cat defaults.conf | while read key value; do
     config[$key]=$value
 done
 
+rm -r $master
 mkdir $master
 cp spark-env.sh $master/
-echo "export SPARK_MASTER_MEMORY=${config["master.mem"]}" > $master/spark-env.sh
+echo ${config["master.mem"]}
+echo "export SPARK_MASTER_MEMORY=${config["master.mem"]}" >> $master/spark-env.sh
 
 # For master
 echo "name ${config["master.name"]}" > $master/config
@@ -22,6 +24,7 @@ echo "workers $workers" >> $master/config
 # For drivers
 for (( i=1 ; i<=$drivers ; i++ ))
 do
+    rm -r driver$1
     mkdir driver$1
     cp spark-env.sh driver$1/
     echo "export SPARK_EXECUTOR_MEMORY=${config["driver.executor_mem"]}" > driver$i/spark-env.sh
@@ -29,6 +32,7 @@ done
 
 for (( i=1 ; i<=$workers ; i++ ))
 do
+    rm -r worker$1
     mkdir worker$1
     cp spark-env.sh worker$1/
     echo "export SPARK_WORKER_MEMORY=${config["worker.mem"]}" >> worker$i/spark-env.sh
