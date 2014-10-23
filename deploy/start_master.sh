456dc9298087584a366631feb9c937e4209b3aa5
LuqmanSahaf <LuqmanSahaf@github.com>
2014-10-23 11:10:39 +0000

    debug

--- deploy/start_master.sh
+++ deploy/start_master.sh
@@ -3,7 +3,7 @@
 master=$1
 private_ip=$2
 
-master_dir=/home/$master
+master_dir=$(pwd)/run/$master
 
 rm -r $master_dir
 mkdir -p $master_dir
@@ -19,4 +19,4 @@ publish_args="-p 8080:8080 -p $private_ip:7077:7077 -p $private_ip:9000:9000"
 env_args="-e ETCD_ADDRESS=$ETCD_IP -e ETCD_PORT=$ETCD_PORT -e HOST_ADDRESS=$private_ip"
 
 # start docker container
-docker run --name $master -h $master $publish_args $env_args -v $master_dir:/home/run spark-master:1.1.0
+docker run -it --name $master -h $master $publish_args $env_args -v $master_dir:/home/run spark-master:1.1.0
